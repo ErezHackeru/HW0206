9b13a5ea9bf6e2056de380ef7bc2aa4099e8c210
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-06-04 12:09:11 +0300

    HW020619 - SyncThreads

---
+++ HW0206_ThreadSync/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HW0206_ThreadSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadExecutor threadExecutor = new ThreadExecutor();
+            threadExecutor.Add(new Thread(CountTo10));
+            threadExecutor.Add(new Thread(PrintHello));
+            threadExecutor.Add(new Thread(PrintShlalom));
+            //Q1 (not etgar) - threadExecutor.Execute();
+            Thread.Sleep(2000);
+            threadExecutor.Add(new Thread(CountTo10));
+            threadExecutor.Add(new Thread(PrintHello));
+            threadExecutor.Add(new Thread(PrintShlalom));
+            Console.ReadKey();
+        }
+        static void CountTo10()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"The number is: {i}");
+                Thread.Sleep(10);
+            }
+        }
+        static void PrintHello()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World !");
+            }            
+        }
+        static void PrintShlalom()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Shalom World !");
+            }
+        }
+    }
+}
---
+++ HW0206_ThreadSync/ThreadExecutor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace HW0206_ThreadSync
+{
+    class ThreadExecutor
+    {
+        Queue<Thread> threads = new Queue<Thread>();
+        //if we use cincurrent queue , delete the lock statements in the methods:
+        //ConcurrentQueue<Thread> threads2 = new ConcurrentQueue<Thread>();
+
+        public ThreadExecutor()
+        {
+            ThreadPool.QueueUserWorkItem((a) =>
+            {
+                Console.WriteLine("Hello from the thread pool.");
+
+                if (threads.Count != 0)
+                {
+                    Execute();
+                }
+            });
+            //System.Timers.Timer timer = new System.Timers.Timer(1000);
+            //timer.Elapsed += OnTimeElapsed;
+            //timer.Enabled = true;
+        }
+
+        private void OnTimeElapsed(object sender, ElapsedEventArgs e)
+        {            
+            new Thread(new ThreadStart(() =>
+            {
+                if (threads.Count != 0)
+                {
+                    Execute();
+                }
+            })).Start();
+        }
+
+        public void Add(Thread thread)
+        {
+            lock (this)
+            {
+                threads.Enqueue(thread);
+            }
+        }
+        public void Execute()
+        {
+            while (threads.Count !=0)
+            {
+                Thread t = threads.Peek();
+                t.Start();
+                t.Join();
+                threads.Dequeue();
+            }
+        }
+    }
+}
